d4598e90e46840a80b85faea8f531baaf39a29d2
Алексей В. Дубовко <Mory@oszn.local>
2022-06-27 16:19:29 +0500

    Второе домашнее задание

---
+++ HW2/DZ2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+string i = Console.ReadLine();
+if (int.Parse (i) > 99)
+{ 
+    Console.WriteLine("Третья цыфра - " + i[2]); 
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
---
+++ HW2/DZ3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введи число дня недели: ");
+int x = Convert.ToInt32(Console.ReadLine());
+if(x < 6)
+{
+    System.Console.WriteLine("Это не выходной день");
+}
+else
+{
+    System.Console.WriteLine("Это выходной день");
+}
