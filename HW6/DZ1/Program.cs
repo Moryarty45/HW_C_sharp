bb46887a08299743b0e89967c3f12181cdbbe43a
Moryarty45 <adubovko@gmail.com>
2022-07-14 16:34:08 +0500

    Сделано ДЗ для 6 семинара

---
+++ HW6/DZ1/Program.cs
@@ -0,0 +1,20 @@
+﻿System.Console.WriteLine("Введите несколько цифр через запятую:");
+
+int[] numbersArray = ReadAndConvertStringToIntArray();
+int[] ReadAndConvertStringToIntArray()
+{
+    return System.Console.ReadLine()!
+        .Split(new[] {","}, StringSplitOptions.RemoveEmptyEntries)
+        .Select(Int32.Parse).ToArray();
+} 
+
+int countGreaterZero = 0;
+foreach (var numbers in numbersArray)
+{
+    if(numbers > 0)
+    {
+        countGreaterZero++;
+    }
+}
+
+System.Console.Write($"Введено чисел больше ноля: {countGreaterZero}");
---
+++ HW6/DZ2/Program.cs
@@ -0,0 +1,21 @@
+﻿System.Console.WriteLine("Введите координаты первой прямой A и B через запятую:");
+double[] firstСoordinatesArray = ReadAndConvertStringToIntArray();
+System.Console.WriteLine("Введите координаты второй прямой A и B через запятую:");
+double[] secondCoorditatesArray = ReadAndConvertStringToIntArray();
+
+double[] ReadAndConvertStringToIntArray()
+{
+    return System.Console.ReadLine()!
+        .Split(new[] {","}, StringSplitOptions.RemoveEmptyEntries)
+        .Select(Double.Parse).ToArray();
+} 
+
+void IntersectionPointTwoLine(double[] firstArray, double[] secondArray)
+{   
+    double x = (firstArray[0] - secondArray[0]) / (secondArray[1] - firstArray[1]);
+    double y = firstArray[1] * x + firstArray[0];
+    System.Console.WriteLine($"Точка пересечения двух прямых: {x};{y}"); 
+    return;
+}
+
+IntersectionPointTwoLine(firstСoordinatesArray, secondCoorditatesArray);
