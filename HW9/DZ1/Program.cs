884728d3ffbba63c65ea30c35b0d8653d22d27eb
Moryarty45 <adubovko@gmail.com>
2022-07-27 07:59:09 +0500

    Сделанное ДЗ для 9го семинара

---
+++ HW9/DZ1/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите число M: ");
+int m = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int SumNaturalElement(int m, int n)
+{
+    if (m == n) return n;
+    else return (m + (SumNaturalElement(m + 1, n)));
+}
+
+System.Console.WriteLine($"Cумма натуральных элементов равняется {SumNaturalElement(m, n)}");
---
+++ HW9/DZ2/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Введите число M: ");
+int m = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int FuncAkkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return FuncAkkerman(m - 1, 1);
+    else if (m > 0 && n > 0) return (FuncAkkerman((m - 1), FuncAkkerman(m,( n - 1))));
+    else return 0;
+}
+
+System.Console.WriteLine($"Функция Аккермана от ({m}, {n}) = {FuncAkkerman(m, n)}");
